7024ae50ddff311081d8dc948cca2048ae9d8bea
Shaapera <shalomshaapera@gmail.com>
2025-01-26 01:58:07 +0100

    task 7

---
+++ script.js
@@ -0,0 +1,35 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const addButton = document.getElementById('add-task-input');
+    const taskInput = document.getElementById("task-input");
+    const taskList = document.getElementById("task-list");
+    function addTask() {
+    
+        if (taskText != "") {
+            const taskListItem = document.createElement('li');
+            taskListItem.textContent = taskText;
+            const removeButton = document.createElement('button');
+            removeButton.textContent = 'Remove';
+            removeButton.classList.add('remove-btn');
+            removeButton.onclick = function () {
+                taskList.removeChild(taskListItem);
+                const taskIndex = task.indexOf(taskText);
+                if (taskIndex !== -1) {
+                    task.splice(taskIndex, 1);
+                }
+                saveTasksToLocalStorage(tasks);
+
+            };
+        } else {
+            alert('enter a task');
+            return;
+        };
+        taskListItem.appendChild(removeButton);
+        taskList.appendChild(taskListItem);
+        tasks.push(taskText);
+        saveTasksToLocalStorage(tasks);
+        taskInput.value = '';
+
+
+
+    }
+})
