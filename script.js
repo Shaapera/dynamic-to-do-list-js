a94def71da734cd7b5bea9202323dcf1822079fa
Shaapera <shalomshaapera@gmail.com>
2025-01-26 02:07:34 +0100

    declared taskText

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ document.addEventListener('DOMContentLoaded', function () {
     const taskInput = document.getElementById("task-input");
     const taskList = document.getElementById("task-list");
     function addTask() {
+        const taskText = taskInput.value.trim();
     
         if (taskText != "") {
             const taskListItem = document.createElement('li');
